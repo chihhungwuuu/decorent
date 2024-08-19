05192c547a6ffa2875255d8df7659da1f346d3fe
Allen Wu <allenwu032016@gmail.com>
2024-08-19 10:53:25 +0800

    remove TestControll and update users sql file

--- 01-starter-files/db-scripts/03-users-and-roles.sql
+++ 01-starter-files/db-scripts/03-users-and-roles.sql
@@ -27,16 +27,18 @@ CREATE TABLE `users` (
 --
 -- NOTE: The passwords are encrypted using BCrypt
 --
--- A generation tool is avail at: http://www.luv2code.com/generate-bcrypt-password
---
--- Default passwords here are: fun123
+-- A generation tool is avail at: https://www.bcryptcalculator.com/
+-- Default passwords here are: fun123,test456
 --
 
 INSERT INTO `users` (`username`,`password`, `email`, `cellphone`)
 VALUES 
 ('john','$2a$04$eFytJDGtjbThXa80FyOOBuFdK2IwjyWefYkMpiBEFlpBwDH.5PM0K','john@gmail.com','0911222333' ),
 ('mary','$2a$04$eFytJDGtjbThXa80FyOOBuFdK2IwjyWefYkMpiBEFlpBwDH.5PM0K','mary@gmail.com','0988412563' ),
-('susan','$2a$04$eFytJDGtjbThXa80FyOOBuFdK2IwjyWefYkMpiBEFlpBwDH.5PM0K','susan@gmail.com','0932541528' );
+('susan','$2a$04$eFytJDGtjbThXa80FyOOBuFdK2IwjyWefYkMpiBEFlpBwDH.5PM0K','susan@gmail.com','0932541528' ),
+('customer','$2a$10$bxFeJIUuBePAe.OWqGdMVu9bpa74o9kozCn3mZ9vVeGAWEefzZfxm','customer@gmail.com','0911222777' ),
+('employee','$2a$10$bxFeJIUuBePAe.OWqGdMVu9bpa74o9kozCn3mZ9vVeGAWEefzZfxm','employee@gmail.com','0988663120' ),
+('admin','$2a$10$bxFeJIUuBePAe.OWqGdMVu9bpa74o9kozCn3mZ9vVeGAWEefzZfxm','admin@gmail.com','0932888541' );
 
 
 --
@@ -97,4 +99,10 @@ VALUES
 (2, 2),
 (3, 1),
 (3, 2),
-(3, 3)
+(3, 3),
+(4, 1),
+(5, 1),
+(5, 2),
+(6, 1),
+(6, 2),
+(6, 3);
