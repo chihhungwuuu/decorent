39b978eb4b18d835a68dfc3b4f31e58e9e10f66d
Allen Wu <allenwu032016@gmail.com>
2024-08-22 13:03:50 +0800

    update db-scripts

--- 01-starter-files/db-scripts/03-users-and-roles.sql
+++ 01-starter-files/db-scripts/03-users-and-roles.sql
@@ -6,6 +6,7 @@ DROP TABLE IF EXISTS `roles`;
 SET foreign_key_checks = 1;
 
 --
+--	使用hibernate創建資料庫
 -- Table structure for table `users`
 --
 
@@ -13,12 +14,10 @@ DROP TABLE IF EXISTS `users`;
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(50) NOT NULL,
-  `password` varchar(80) NOT NULL,
-  `enabled` tinyint NOT NULL,  
-  `first_name` varchar(64) NOT NULL,
-  `last_name` varchar(64) NOT NULL,
+  `cellphone` varchar(50) NOT NULL,
   `email` varchar(64) NOT NULL,
+  `password` varchar(80) NOT NULL,
+  `username` varchar(80) NOT NULL
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
 
@@ -28,11 +27,14 @@ CREATE TABLE `users` (
 -- NOTE: The passwords are encrypted using BCrypt
 --
 -- A generation tool is avail at: https://www.bcryptcalculator.com/
--- Default passwords here are: test456
+-- Default passwords here are: fun123,test456
 --
 
 INSERT INTO `users` (`username`,`password`, `email`, `cellphone`)
 VALUES 
+('john','$2a$04$eFytJDGtjbThXa80FyOOBuFdK2IwjyWefYkMpiBEFlpBwDH.5PM0K','john@gmail.com','0911222333' ),
+('mary','$2a$04$eFytJDGtjbThXa80FyOOBuFdK2IwjyWefYkMpiBEFlpBwDH.5PM0K','mary@gmail.com','0988412563' ),
+('susan','$2a$04$eFytJDGtjbThXa80FyOOBuFdK2IwjyWefYkMpiBEFlpBwDH.5PM0K','susan@gmail.com','0932541528' ),
 ('customer','$2a$10$bxFeJIUuBePAe.OWqGdMVu9bpa74o9kozCn3mZ9vVeGAWEefzZfxm','customer@gmail.com','0911222777' ),
 ('employee','$2a$10$bxFeJIUuBePAe.OWqGdMVu9bpa74o9kozCn3mZ9vVeGAWEefzZfxm','employee@gmail.com','0988663120' ),
 ('admin','$2a$10$bxFeJIUuBePAe.OWqGdMVu9bpa74o9kozCn3mZ9vVeGAWEefzZfxm','admin@gmail.com','0932888541' );
@@ -96,5 +98,10 @@ VALUES
 (2, 2),
 (3, 1),
 (3, 2),
-(3, 3);
-
+(3, 3),
+(4, 1),
+(5, 1),
+(5, 2),
+(6, 1),
+(6, 2),
+(6, 3);
